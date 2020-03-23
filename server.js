4965e9a7b569f6b44b8c1424b5eca2d09b5e0193
smganeles <50884841+smganeles@users.noreply.github.com>
2020-03-22 21:19:49 -0400

    three_fourths done writing game

--- server.js
+++ server.js
@@ -9,19 +9,137 @@ var io = socketIO(server);
 
 app.set('port',5000);
 app.use('/static', express.static(__dirname + '/static')); //Routing
+server.listen(5000, function() {
+  console.log('Starting server on port 5000');
+});
+
+///////////////////////////////////////////
 
 app.get('/', function(request,response){
-	response.sendFile(path.join(__dirname, 'index.html'));
-}); //starts server
+	response.sendFile(path.join(__dirname, 'pages/login.html'));
+}); 
 
-server.listen(5000, function() {
-	console.log('Starting server on port 5000');
+app.get('/game', function(req,res){
+  res.sendFile(path.join(__dirname, 'pages/index.html'))
 });
 
-// Add the WebSocket handlers
+
+//////////////////////////////////////////
+
+// let letters_rem = {
+//   A: 13, B: 3, C: 3, D: 6, E: 18, F: 3, G: 4, H: 3, I: 12,
+//   J: 2, K: 2, L: 5, M: 3, N: 8, O: 11, P: 4, Q: 2, R: 9,
+//   S: 6, T: 9, U: 6, V: 3, W: 3, X: 2, Y: 3, Z: 2
+// }
+let letters_rem = [
+  "A","A","A","A","A","A","A","A","A","A","A","A","A",
+  "B","B","B",
+  "C","C","C",
+  "D","D","D","D","D","D",
+  "E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E",
+  "F","F","F",
+  "G","G","G","G",
+  "H","H","H",
+  "I","I","I","I","I","I","I","I","I","I","I","I",
+  "J","J",
+  "K","K",
+  "L","L","L","L","L",
+  "M","M","M",
+  "N","N","N","N","N","N","N","N",
+  "O","O","O","O","O","O","O","O","O","O","O",
+  "P","P","P","P",
+  "Q","Q",
+  "R","R","R","R","R","R","R","R","R",
+  "S","S","S","S","S","S",
+  "T","T","T","T","T","T","T","T","T",
+  "U","U","U","U","U","U",
+  "V","V","V",
+  "W","W","W",
+  "X","X",
+  "Y","Y","Y",
+  "Z","Z"]
+
+var flip_timer = 10000;
+
+// var state = {
+//   letter_bank: [],
+//   players: {},
+//   animations: []
+// };
+
+var state = {
+  letter_bank: [],
+  players: {
+    Simon: ["QUA","HOLA","HELLO"],
+    Elan: ["ACUTE", "WHELP"],
+    Gabi: ["MILK","EGGS", "CEREAL"],
+    Fourth: ["PURPLE","KNIFE","KNIGHT","REDEWIFENER"]
+  },
+  animations: []
+};
+
+var id_to_players = {
+}
+
+
+setInterval(function() {  //letter flipping
+  var num_remain = letters_rem.length;
+  if (num_remain > 0) {
+    var rand_idx = Math.floor(Math.random()*num_remain);
+    var letter = letters_rem[rand_idx];
+    letters_rem.splice(rand_idx,1);
+    state["letter_bank"].push(letter);
+    refresh();
+  }
+}, flip_timer);
+
+
+
 io.on('connection', function(socket) {
+  socket.on('new player', function() {
+    // state[players][socket.id] = [];
+    refresh();
+  });
+
+  socket.on('word_submit', function(data) {
+    var valid = true;
+    //TODO
+    //check if word in dictionary
+    //check if over 3 letters
+    var word = data["word"].toUpperCase();
+    var player = data["id"];
+    var letters = state["letter_bank"].slice();
+    for (letter of word) {
+      if (letters.includes(letter)) { //remove it and continue iterating
+        const index = letters.indexOf(letter);
+        letters.splice(index,1);
+      } else {
+        socket.emit('alert',"not enough letters")
+        valid = false;
+        break
+      }
+    }
+    if (valid == true) {
+      state["letter_bank"] = letters;
+      state["players"][player].push(word);
+      io.sockets.emit('msg',player+" took "+word);
+    }
+    refresh();
+  });
+
+  socket.on('steal', function(data) {
+    console.log(data);
+    //1) if valid steal (1 or more extra letter), letters in word bank
+    //2) if new word in dictionary
+    //3) if not same root
+    //then: player steals word
+    refresh();
+  });
+
 });
 
-setInterval(function() {
-  io.sockets.emit('message', 'hi!');
-}, 1000);
+
+function refresh() {
+  io.sockets.emit('state', state);
+  state["animations"] = [];
+}
--- static/game.js
+++ static/game.js
@@ -1,7 +1,127 @@
 var socket = io();
-socket.on('message', function(data) {
-  console.log(data);
-  $("#p1").html("HOLA AMIGOS");
-  document.getElementById("p1").innerHTML = 'HI THERE';
-  // $("#p1").innerHTML = 'hi there';
-});
+
+var id="Simon";
+
+var active;
+var stealing=false;
+var player_from;
+var steal_word;
+
+function new_word() {
+	$(".word").css("border","none");
+	active = null;
+	$("#submission").focus();
+	stealing=false;
+}
+
+function Stealing(word) {
+	stealing=true;
+	var word_obj = $("#"+word);
+	active = word;
+	$(".word").css("border","none");
+	word_obj.css("border", "2px solid white");
+	word_obj.css("border-radius","4px");
+	player_from = word_obj.parent().attr("id");
+	steal_word = word;
+	$("#submission").focus();
+}
+
+$(document).ready(function() {
+
+	socket.emit('new player');
+
+	$("#submit").click(function(){ //submit a word
+		var word = $("#submission").val();
+		$("#submission").val("");
+		if (stealing == false) {
+			package = {"word":word,"id":id};
+			socket.emit('word_submit',package);
+		} else { //stealing a word
+			package = {
+				"new_word":word,
+				"steal_word":steal_word,
+				"player_from":player_from,
+				"player_to":id}
+			socket.emit('steal',package);
+		}
+		stealing=false;
+		active=null;
+		player_from=null;
+		steal_word=null;
+	});
+
+	$("#submission").on("keyup", function(event) { //submit word when press enter
+  		if (event.keyCode === 13) { 
+		    // Cancel the default action, if needed
+		    event.preventDefault();
+		    $("#submit").click();
+		}
+	});
+
+
+
+	//update the html
+	socket.on('state', function(state) {
+		//update words
+		for (player in state["players"]) {
+			if (!document.getElementById(player)) {  //if the player isn't on the board yet
+				if (player!=id) {
+					$("#top-row").append(
+						"<div class = 'col word-bank' id = '" + player + "'></div>");
+				} else {
+					$("#bottom-row").append(
+						"<div class = 'col word-bank' id = '" + player + "'></div>");
+				}
+			}
+			$("#"+player).html("<h5>"+player+"</h5>"); //clear all words
+
+			for (word of state["players"][player]) {  //redraw all words
+				var word_HTML = "";
+				for (letter of word) {
+					word_HTML += "<p class = 'letter'>" + letter + "</p>";
+				}
+				$("#"+player).append(
+					"<button class = 'word px-0' onclick = 'Stealing("+'"'+word+'"'+")' id = '" + word + "'>" + word_HTML + "</button>");
+			}
+		}
+		//update letter bank
+		$("#letter-bank").html("");
+		for (letter of state["letter_bank"]) {  //redraw all letters in letter-bank
+			$("#letter-bank").append("<p class = 'letter m-2'>" + letter + "</p>");
+		}
+		//reoutline active word
+		if (active) {
+			$("#"+active).css("border", "2px solid white");
+			$("#"+active).css("border-radius","4px");
+		}
+		//fix the height of the log
+		var log = $("#log");
+		var msgs = log.html();
+		log.html("");
+		var height=$("#letter-bank").innerHeight();
+		$("#log").css("max-height",height);
+		$("#log").html(msgs);
+	});
+
+	socket.on('msg',function(msg) {
+		$("#log").append(
+			"<p class='msg'>" + msg + "</p>");
+	});
+
+	socket.on('alert',function(alert) {
+		$("#log").append(
+			"<p class='msg alrt'>" + alert + "</p>");
+	});
+
+
+
+
+
+}); //end doc.ready
+
+
+
+
+
+
+
