e4883839eeddbd553fc087c9e431c4b381b4fa47
smganeles <50884841+smganeles@users.noreply.github.com>
2020-03-30 17:45:15 -0400

    removed commented out letter set and deleted login.html

--- server.js
+++ server.js
@@ -32,35 +32,6 @@ app.get('/restart', function(req,res){
 //////////////////////////////////////////
 
 let wordset = new Set(fs.readFileSync(path.join(__dirname,'static/wordlist.txt'),'utf8').split("\n"));
-
-// let letters_rem = [
-//   "A","A","A","A","A","A","A","A","A","A","A","A","A",
-//   "B","B","B",
-//   "C","C","C",
-//   "D","D","D","D","D","D",
-//   "E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E",
-//   "F","F","F",
-//   "G","G","G","G",
-//   "H","H","H",
-//   "I","I","I","I","I","I","I","I","I","I","I","I",
-//   "J","J",
-//   "K","K",
-//   "L","L","L","L","L",
-//   "M","M","M",
-//   "N","N","N","N","N","N","N","N",
-//   "O","O","O","O","O","O","O","O","O","O","O",
-//   "P","P","P","P",
-//   "Q","Q",
-//   "R","R","R","R","R","R","R","R","R",
-//   "S","S","S","S","S","S",
-//   "T","T","T","T","T","T","T","T","T",
-//   "U","U","U","U","U","U",
-//   "V","V","V",
-//   "W","W","W",
-//   "X","X",
-//   "Y","Y","Y",
-//   "Z","Z"];
-
 let letters_rem = fs.readFileSync(path.join(__dirname,'static/letters.txt'),'utf8').split(",");
 const tiles_list = fs.readFileSync(path.join(__dirname,'static/letters.txt'),'utf8').split(",");
 let flip_timer = 8000;
@@ -80,7 +51,6 @@ let letter_flip;
 let single_flip;
 let flipping = false;
 let chosen;
-
 
 
 //////////////////////////////////////////
